08c8b14eaa0f13d52a7141a4edd32a544d492a14
hana2set <devkim4251@gmail.com>
2025-09-04 17:42:41 +0900

    자동화 제어 메세지 제거, ChromeDriverManager 삭제 (selenium 안에 내장되었다고 함.)

--- web_util.py
+++ web_util.py
@@ -1,9 +1,7 @@
 import logging
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 from config import config
 from session_manager import session_manager
@@ -12,24 +10,24 @@ driver = None
 
 
 def init_driver():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")  # 시크릿 모드
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-gpu")  # GPU 가속 비활성화
     chrome_options.add_argument("--disable-extensions")  # 확장 프로그램 비활성화
     chrome_options.add_argument("--disable-dev-shm-usage")  # 공유 메모리 비활성
-    chrome_options.add_argument("--disable-automation")  # 자동화된 테스트 소프트웨어에 의해 제어되고 있다는 메세지 제거
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # 자동화 도구 방지
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_experimental_option("detach", True)
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    service = Service(ChromeDriverManager().install())
-    return webdriver.Chrome(service=service, options=chrome_options)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])  # 콘솔로그 출력 안하게
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])  # 자동화 제어 메시지 제거
+
+    return webdriver.Chrome(service=Service(), options=chrome_options)
 
 
 def open_web_mail():
     global driver
 
+    # chromedriver 서비스 종료되지 않는 현상 제거
     if driver is not None:
         driver.quit()
 
